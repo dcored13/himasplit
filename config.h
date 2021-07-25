afda8ace51d6eba6f117e1b9a2db2ca3b7f3be30
dcored13 <danielncharlton.dc@gmail.com>
2021-07-25 11:28:30 +1000

    Add files via upload

--- config.h
+++ config.h
@@ -5,27 +5,28 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xDC0D
-#define PRODUCT_ID      0x4A42
+#define PRODUCT_ID      0x4A41
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    dcored
-#define PRODUCT         himasplit
-#define DESCRIPTION     split himawari board
-
+#define PRODUCT         himawari
+#define DESCRIPTION     split fixed
 
 /* key matrix size */
 #define MATRIX_ROWS 5
-#define MATRIX_COLS 14
+#define MATRIX_COLS 13
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { D3, D2, D1, D4, C6 }
-#define MATRIX_COL_PINS { D7, E6, B4, B5, B6, B2, B3 }
+#define MATRIX_ROW_PINS { D3, D2, D1, D0, D4 }
+#define MATRIX_COL_PINS { C6, D7, E6, B4, B5, B6, B2, B3, B1, F7, F6, F5, F4 }
+#define UNUSED_PINS
 
+/* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-#define DEBOUNCE 5
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCING_DELAY 5
 
-#define SOFT_SERIAL_PIN D0
-
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 
 /* Locking resynchronize hack */
